d2127909d89367bcfe67f0277c84e1dd30758a01
Sumia Iqbal <iqbalsumia30@gmail.>
2025-08-12 06:09:40 -0700

    shifted to new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:food_scanner_app/widgets/main_nav.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home:MainNav(),
       debugShowCheckedModeBanner: false,
     );
   }
